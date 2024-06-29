ed3707a4f46461c5b64e72df39230181d4821051
Jonathan <jonathanking.dev@gmail.com>
2024-06-29 12:38:01 +0200

    Call api to get tasklist

--- src/pages/task-list/TaskListPage.tsx
+++ src/pages/task-list/TaskListPage.tsx
@@ -1,7 +1,24 @@
+import { TaskModel } from "@/services/api/models/task.api-model";
+import { useGetTasks } from "@/services/api/requests/getTask.api";
 import React from "react";
 
 export const TaskListPage: React.FC = () => {
-  return <h1>HOOLA DESDE LA TASKLIST</h1>;
+  const { data } = useGetTasks();
+  console.log(data);
+  return (
+    <>
+      <div>
+        <h1>TASK LIST</h1>
+        <div>
+          <ul>
+            {data?.map((task: TaskModel) => (
+              <li key={task.id}>{task.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 //TODO: añadir un header con el nombre del usuario y un boton de logout
